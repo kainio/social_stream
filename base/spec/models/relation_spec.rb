3e468274f98f6a504b1b2d8c2111deabcdf50259
Soufiane El Kassib <poliamus@yahoo.com>
2018-03-06 13:21:09 +0000

    Make models/relation_spec.rb spec pass

--- base/spec/models/relation_spec.rb
+++ base/spec/models/relation_spec.rb
@@ -3,96 +3,96 @@ require 'rails_helper'
 describe Relation do
   context "authorization" do
     before(:all) do
-      @tie = Factory(:friend)
+      @tie = FactoryBot.create(:friend)
       @relation = @tie.relation
     end
 
     describe ", receiver" do
-      before do
+      before(:each) do
         @s = @tie.receiver
       end
 
       it "creates activity" do
-        Relation.allow(@s, 'create', 'activity').should include(@relation)
+        expect(Relation.allow(@s, 'create', 'activity')).to include(@relation)
       end
 
       it "reads activity" do
-        Relation.allow(@s, 'read', 'activity').should include(@relation)
+        expect(Relation.allow(@s, 'read', 'activity')).to include(@relation)
       end
     end
 
     describe ", acquaintance" do
-      before do
-        @s = Factory(:acquaintance, :contact => Factory(:contact, :sender => @tie.sender)).receiver
+      before(:each) do
+        @s = FactoryBot.create(:acquaintance, :contact => FactoryBot.create(:contact, :sender => @tie.sender)).receiver
       end
 
       it "creates activity" do
-        Relation.allow(@s, 'create', 'activity').should_not include(@relation)
+        expect(Relation.allow(@s, 'create', 'activity')).to_not include(@relation)
       end
 
       it "reads activity" do
-        Relation.allow(@s, 'read', 'activity').should_not include(@relation)
+        expect(Relation.allow(@s, 'read', 'activity')).to_not include(@relation)
       end
     end
 
     describe ", alien" do
-      before do
-        @s = Factory(:user)
+      before(:each) do
+        @s = FactoryBot.create(:user)
       end
 
       it "creates activity" do
-        Relation.allow(@s, 'create', 'activity').should_not include(@relation)
+        expect(Relation.allow(@s, 'create', 'activity')).to_not include(@relation)
       end
       
       it "reads activity" do
-        Relation.allow(@s, 'read', 'activity').should_not include(@relation)
+        expect(Relation.allow(@s, 'read', 'activity')).to_not include(@relation)
       end
     end
   end
 
   describe "member" do
-    before do
-      @tie = Factory(:member)
+    before(:each) do
+      @tie = FactoryBot.create(:member)
       @relation = @tie.relation
     end
 
     describe ", member" do
-      before do
-        @s = Factory(:member, :contact => Factory(:contact, :sender => @tie.sender)).receiver
+      before(:each) do
+        @s = FactoryBot.create(:member, :contact => FactoryBot.create(:contact, :sender => @tie.sender)).receiver
       end
 
       it "creates activity" do
-        Relation.allow(@s, 'create', 'activity').should include(@relation)
+        expect(Relation.allow(@s, 'create', 'activity')).to include(@relation)
       end
 
        it "reads activity" do
-        Relation.allow(@s, 'read', 'activity').should include(@relation)
+        expect(Relation.allow(@s, 'read', 'activity')).to include(@relation)
       end
 
       it "updates activity" do
-        Relation.allow(@s, 'update', 'activity').should_not include(@relation)
+        expect(Relation.allow(@s, 'update', 'activity')).to_not include(@relation)
       end
     end
   end
 
   describe 'with follow permission' do
-    before do
+    before(:each) do
       @relation = Relation.create!
-      @relation.permissions << Permission.find_or_create_by_action('follow')
+      @relation.permissions << Permission.find_or_create_by(action: 'follow')
     end
 
     it 'should follow?' do
-      @relation.follow?.should be_true
+      expect(@relation.follow?).to be true
     end
   end
 
   describe 'without follow permission' do
-    before do
+    before(:each) do
       @relation = Relation.create!
     end
 
     it 'should follow?' do
-      @relation.follow?.should be_false
+      expect(@relation.follow?).to be false
     end
   end
 
